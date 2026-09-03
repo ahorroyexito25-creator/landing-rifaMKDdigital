4cad02addcb6ec482bdabc42ea4cc875d380030a
ahorroyexito25-creator <ahorroyexito25@gmail.com>
2026-09-02 21:22:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG_LANDING = {
-    loteriaNombre: "GRAN RIFA MEDELLÍN",
-    fechaSorteo: "Miércoles 2 de Septiembre de 2026, 8:00 p.m.",
-    premioMayor: "700.000 $ COP",
-    valorBoleto: "$10.000 COP",
+    loteriaNombre: "GRAN RIFA CHONTICO NOCHE",
+    fechaSorteo: "Miércoles 19 de Septiembre de 2026, 10:00 p.m.",
+    premioMayor: "1.200.000 $ COP",
+    valorBoleto: "$20.000 COP",
     pais: "Colombia 🇨🇴",
-    urlAppRifa: "https://rifa-mkddigital-p.up.railway.app",
+    urlAppRifa: "https://rifa-mkddigital.onrender.com",
     whatsappContacto: "https://wa.me/573150000000?text=Hola,%20quiero%20apartar%20mis%20números%20para%20la%20rifa"
 };
