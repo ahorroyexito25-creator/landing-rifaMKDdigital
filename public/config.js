52315c59fb3a025807e5efa2bb6505d77e1debd7
ahorroyexito25-creator <ahorroyexito25@gmail.com>
2026-09-02 19:11:21 -0500

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,9 @@
+const CONFIG_LANDING = {
+    loteriaNombre: "GRAN RIFA MEDELLÍN",
+    fechaSorteo: "Miércoles 2 de Septiembre de 2026, 8:00 p.m.",
+    premioMayor: "700.000 $ COP",
+    valorBoleto: "$10.000 COP",
+    pais: "Colombia 🇨🇴",
+    urlAppRifa: "https://rifa-mkddigital-p.up.railway.app",
+    whatsappContacto: "https://wa.me/573150000000?text=Hola,%20quiero%20apartar%20mis%20números%20para%20la%20rifa"
+};
